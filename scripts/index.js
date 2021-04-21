0319df3c11eafa20b3e99039d9c93e6a259aba1f
Dmitry Goryansky <gorianskyd@yandex.ru>
2021-04-21 17:41:32 +0800

    испарил замечания после первого коммита

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,29 @@
 let openPopupButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closePopupButton = document.querySelector('.popup__close');
-
-function togglePopup() {
-    popup.classList.toggle('popup_opened');
-}
-
-openPopupButton.addEventListener('click', togglePopup);
-closePopupButton.addEventListener('click', togglePopup);
-
-let formElement = document.querySelector('.input');
-let nameInput = document.querySelector('.input__name');
-let jobInput = document.querySelector('.input__job');
+let formElement = document.querySelector('.popup__container');
+let nameInput = document.querySelector('.popup__input_data_user-name');
+let jobInput = document.querySelector('.popup__input_data_job');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
-nameInput.value = profileName.textContent;
-jobInput.value = profileJob.textContent;
+
+function togglePopupOpen() {
+    popup.classList.toggle('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+}
+
+function togglePopupClose() {
+    popup.classList.toggle('popup_opened');
+}
 
 function formSubmitHandler(event) {
     event.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-    togglePopup();
+    togglePopupClose();
 }
+
+openPopupButton.addEventListener('click', togglePopupOpen);
+closePopupButton.addEventListener('click', togglePopupClose);
 formElement.addEventListener('submit', formSubmitHandler);
