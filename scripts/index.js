cf84214ae47fd0e80d07058a2a6b8bdb3d8ee220
Dmitry Goryansky <gorianskyd@yandex.ru>
2021-05-03 17:23:50 +0800

    написал код для загрузки карточек из массива

--- scripts/index.js
+++ scripts/index.js
@@ -9,7 +9,47 @@ let profileJob = document.querySelector('.profile__job');
 const openNewLocationButton = document.querySelector('.profile__add-button');
 const newLocation = document.querySelector('.new-location');
 const closeNewLocationButton = newLocation.querySelector('.popup__close');
-
+const galleryList = document.querySelector('.gallery__elements');
+const elementTemplate = document.querySelector('.template-element').content;
+//массив имена фотографий и ссылок к ним
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 
+//вставка карточек при загрузке страницы
+initialCards.forEach(insertingСards);
+//заполняем карточки данными
+function insertingСards(el) {
+    const galleryElement = elementTemplate.cloneNode(true);
+    galleryElement.querySelector('.element__photo').src = el.link;
+    galleryElement.querySelector('.element__photo-title').textContent = el.name;
+    galleryElement.querySelector('.element__photo').alt = el.name;
+    
+    galleryList.append(galleryElement);
+}
+//открываем и закрываем форму редактирования профиля
 function togglePopupOpen() {
     popup.classList.toggle('popup_opened');
     nameInput.value = profileName.textContent;
@@ -19,14 +59,14 @@ function togglePopupOpen() {
 function togglePopupClose() {
     popup.classList.toggle('popup_opened');
 }
-
+//сохранение с закрытием формы редактирования профиля
 function formSubmitHandler(event) {
     event.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
     togglePopupClose();
 }
-
+//открываем и закрываем форму добавления фотографий
 function newLocationOpen() {
     newLocation.classList.toggle('popup_opened');
 }
@@ -34,7 +74,7 @@ function newLocationOpen() {
 function newLocationClose() {
     newLocation.classList.toggle('popup_opened');
 }
-
+//слушаем кнопки
 openPopupButton.addEventListener('click', togglePopupOpen);
 closePopupButton.addEventListener('click', togglePopupClose);
 formElement.addEventListener('submit', formSubmitHandler);
