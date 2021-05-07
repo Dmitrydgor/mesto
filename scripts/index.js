6b7d4460ed4cfdf8968d31eea3843e20586e4ace
Dmitry Goryansky <gorianskyd@yandex.ru>
2021-05-07 15:07:34 +0800

    отформатировал код

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
-const openPopupButton = document.querySelector('.profile__edit-button');
+const openProfileEditButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
-const closePopupButton = document.querySelector('.popup__close'); 
+const closeProfileEditButton = document.querySelector('.popup__close');
 const editForm = document.querySelector('.edit-profile__form');
 const newLocationForm = document.querySelector('.new-location__form');
 const nameInput = document.querySelector('.popup__input_data_user-name');
@@ -17,108 +17,100 @@ const locationLink = document.querySelector('.popup__input_data_link');
 const viewPhoto = document.querySelector('.popup-photo');
 const closePhotoButton = viewPhoto.querySelector('.popup__close');
 //массив имена фотографий и ссылок к ним
-const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+const initialCards = [{
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
     },
     {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
     },
     {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
     },
     {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
     },
     {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
     },
     {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
-  ]; 
-//вставка карточек при загрузке страницы
-initialCards.forEach(insertingСards);
+];
 //заполняем карточки данными
-function insertingСards(el) {
-  const galleryElement = elementTemplate.cloneNode(true);
-  galleryElement.querySelector('.element__photo').src = el.link;
-  galleryElement.querySelector('.element__photo-title').textContent = el.name;
-  galleryElement.querySelector('.element__photo').alt = el.name;
-  const elementLike = galleryElement.querySelector('.element__like');
-  elementLike.addEventListener('click', () => elementLike.classList.toggle('element__like_active'));
-  const trashElement = galleryElement.querySelector('.element__trash');
-  trashElement.addEventListener('click', deleteElement);
-  const openPhotoButton = galleryElement.querySelector('.element__photo');
-  function viewPhotoOpen() {
-    viewPhoto.classList.add('popup_opened');
-    viewPhoto.querySelector('.popup__photo').src = el.link;
-    viewPhoto.querySelector('.popup-photo__title').textContent = el.name;
-    viewPhoto.querySelector('.popup__photo').alt = el.name;
-  }
-  openPhotoButton.addEventListener('click', viewPhotoOpen);
-  closePhotoButton.addEventListener('click', viewPhotoClose);
-  galleryList.append(galleryElement);
-}
-//открываем и закрываем фото
-function viewPhotoClose() {
-  viewPhoto.classList.remove('popup_opened');
-}
-//открываем и закрываем форму редактирования профиля
-function togglePopupOpen() {
-  popup.classList.toggle('popup_opened');
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
-}
+function createCard(el) {
+    const galleryElement = elementTemplate.cloneNode(true);
+    galleryElement.querySelector('.element__photo').src = el.link;
+    galleryElement.querySelector('.element__photo-title').textContent = el.name;
+    galleryElement.querySelector('.element__photo').alt = el.name;
 
-function togglePopupClose() {
-  popup.classList.toggle('popup_opened');
+    const elementLike = galleryElement.querySelector('.element__like');
+    elementLike.addEventListener('click', () => elementLike.classList.toggle('element__like_active'));
+
+    const trashElement = galleryElement.querySelector('.element__trash');
+    trashElement.addEventListener('click', deleteElement);
+
+    const openPhotoButton = galleryElement.querySelector('.element__photo');
+    openPhotoButton.addEventListener('click', function viewPhotoOpen() {
+        viewPhoto.querySelector('.popup__photo-img').src = el.link;
+        viewPhoto.querySelector('.popup__caption').textContent = el.name;
+        viewPhoto.querySelector('.popup__photo-img').alt = el.name;
+        togglePopup(viewPhoto);
+    });
+    return galleryElement;
 }
-//сохранение с закрытием формы редактирования профиля
+closePhotoButton.addEventListener('click', () => togglePopup(viewPhoto));
+//вставка карточек при загрузке страницы
+initialCards.forEach(function(currentItem) {
+    const newCard = createCard(currentItem);
+    galleryList.append(newCard);
+});
+
+function togglePopup(popup) {
+    popup.classList.toggle('popup_opened');
+}
+//сохраняет и закрывает форму редактирования профиля
 function profileFormSubmitHandler(event) {
-  event.preventDefault();
-  profileName.textContent = nameInput.value;
-  profileJob.textContent = jobInput.value;
-  togglePopupClose();
+    event.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+    togglePopup(popup);
 }
-//открываем и закрываем форму добавления фотографий
-function newLocationOpen() {
-  newLocation.classList.toggle('popup_opened');
-}
-
-function newLocationClose() {
-  newLocation.classList.toggle('popup_opened');
-}
-//сохранение с закрытием формы добавления фото
+//сохраняет и закрывает форму добавления фото
 function newLocationFormSubmitHandler(event) {
-  event.preventDefault();
-  const newGalleryElement = elementTemplate.cloneNode(true);
-  newGalleryElement.querySelector('.element__photo').src = locationLink.value;
-  newGalleryElement.querySelector('.element__photo-title').textContent = locationName.value;
-  newGalleryElement.querySelector('.element__photo').alt = locationName.value;
-  galleryList.append(newGalleryElement);
-  event.target.reset();
-  newLocationClose();
-}
-//ставим лайк
-function likeElement() {
-  elementLike.target.classList.toggle('element__like_active');
-  console.log(likeElement);
+    event.preventDefault();
+    const newCard = {
+        name: locationName.value,
+        link: locationLink.value
+    };
+    const card = createCard(newCard);
+
+    galleryList.prepend(card);
+    event.target.reset();
+    togglePopup(newLocation);
 }
 //удаление карточки
 function deleteElement(e) {
-  e.target.closest('.element').remove();
+    e.target.closest('.element').remove();
 }
-//слушаем кнопки
-openPopupButton.addEventListener('click', togglePopupOpen);
-closePopupButton.addEventListener('click', togglePopupClose);
+//открывает форму редактирования профиля и заполняет из html
+openProfileEditButton.addEventListener('click', function copyProfile() {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+    togglePopup(popup);
+});
+//закрывает форму редактирования профиля
+closeProfileEditButton.addEventListener('click', () => togglePopup(popup));
+//сохраняет форму редактирования профиля
 editForm.addEventListener('submit', profileFormSubmitHandler);
-openNewLocationButton.addEventListener('click', newLocationOpen);
-closeNewLocationButton.addEventListener('click', newLocationClose);
+//открывает форму добавления места
+openNewLocationButton.addEventListener('click', () => togglePopup(newLocation));
+//закрывает форму добавления места
+closeNewLocationButton.addEventListener('click', () => togglePopup(newLocation));
+//сохраняет форму добавления места
 newLocationForm.addEventListener('submit', newLocationFormSubmitHandler);
